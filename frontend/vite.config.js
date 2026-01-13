9d5afc17f02a1ed8f2a5127473da33bfcaaa7ce2
Nurevsan Kurak <nurevsan@Nurevsan-MacBook-Air.local>
2026-01-13 06:58:17 +0300

    Fix Vite base path for DigitalOcean

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/smartkitchen-frontend/',
   plugins: [react()],
-  base: '/',
   server: {
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+      },
+    },
+  },
 })
